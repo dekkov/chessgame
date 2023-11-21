b2400ff4b91522285330d83ff489fba0974d5bed
Hoang Tran <trhoang2003@gmail.com>
2023-11-21 00:06:10 -0800

    Create piece.py
    Piece & BIshop

---
+++ piece.py
@@ -0,0 +1,157 @@
+import pygame
+import os
+
+b_bishop = pygame.image.load(os.path.join("img", "black_bishop.png"))
+b_king = pygame.image.load(os.path.join("img", "black_king.png"))
+b_knight = pygame.image.load(os.path.join("img", "black_knight.png"))
+b_pawn = pygame.image.load(os.path.join("img", "black_pawn.png"))
+b_queen = pygame.image.load(os.path.join("img", "black_queen.png"))
+b_rook = pygame.image.load(os.path.join("img", "black_rook.png"))
+
+w_bishop = pygame.image.load(os.path.join("img", "white_bishop.png"))
+w_king = pygame.image.load(os.path.join("img", "white_king.png"))
+w_knight = pygame.image.load(os.path.join("img", "white_knight.png"))
+w_pawn = pygame.image.load(os.path.join("img", "white_pawn.png"))
+w_queen = pygame.image.load(os.path.join("img", "white_queen.png"))
+w_rook = pygame.image.load(os.path.join("img", "white_rook.png"))
+
+b = [b_bishop, b_king, b_knight, b_pawn, b_queen, b_rook]
+w = [w_bishop, w_king, w_knight, w_pawn, w_queen, w_rook]
+
+B = []
+W = []
+
+for img in b:
+    B.append(pygame.transform.scale(img, (55, 55)))
+
+for img in w:
+    W.append(pygame.transform.scale(img, (55, 55)))
+
+
+class Piece:
+    img = -1
+    rect = (113, 113, 525, 525)
+    startX = rect[0]
+    startY = rect[1]
+
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+        self.selected = False
+        self.move_list = []
+        self.king = False
+        self.pawn = False
+
+    def isSelected(self):
+        return self.selected
+
+    def update_valid_moves(self, board):
+        self.move_list = self.valid_moves(board)
+
+    def draw(self, win, color):
+        if self.color == "w":
+            drawThis = W[self.img]
+        else:
+            drawThis = B[self.img]
+
+        x = (4 - self.col) + round(self.startX + (self.col * self.rect[2] / 8))
+        y = 3 + round(self.startY + (self.row * self.rect[3] / 8))
+
+        if self.selected and self.color == color:
+            pygame.draw.rect(win, (255, 0, 0), (x, y, 62, 62), 4)
+
+        win.blit(drawThis, (x, y))
+
+        '''if self.selected and self.color == color:  # Remove false to draw dots
+            moves = self.move_list
+
+            for move in moves:
+                x = 33 + round(self.startX + (move[0] * self.rect[2] / 8))
+                y = 33 + round(self.startY + (move[1] * self.rect[3] / 8))
+                pygame.draw.circle(win, (255, 0, 0), (x, y), 10)'''
+
+    def change_pos(self, pos):
+        self.row = pos[0]
+        self.col = pos[1]
+
+    def __str__(self):
+        return str(self.col) + " " + str(self.row)
+
+
+class Bishop(Piece):
+    img = 0
+
+    def valid_moves(self, board):
+        i = self.row
+        j = self.col
+
+        moves = []
+
+        # TOP RIGHT
+        djL = j + 1
+        djR = j - 1
+        for di in range(i - 1, -1, -1):
+            if djL < 8:
+                p = board[di][djL]
+                if p == 0:
+                    moves.append((djL, di))
+                elif p.color != self.color:
+                    moves.append((djL, di))
+                    break
+                else:
+                    break
+            else:
+                break
+
+            djL += 1
+
+        for di in range(i - 1, -1, -1):
+            if djR > -1:
+                p = board[di][djR]
+                if p == 0:
+                    moves.append((djR, di))
+                elif p.color != self.color:
+                    moves.append((djR, di))
+                    break
+                else:
+                    break
+            else:
+                break
+
+            djR -= 1
+
+        # TOP LEFT
+        djL = j + 1
+        djR = j - 1
+        for di in range(i + 1, 8):
+            if djL < 8:
+                p = board[di][djL]
+                if p == 0:
+                    moves.append((djL, di))
+                elif p.color != self.color:
+                    moves.append((djL, di))
+                    break
+                else:
+                    break
+            else:
+                break
+            djL += 1
+        for di in range(i + 1, 8):
+            if djR > -1:
+                p = board[di][djR]
+                if p == 0:
+                    moves.append((djR, di))
+                elif p.color != self.color:
+                    moves.append((djR, di))
+                    break
+                else:
+                    break
+            else:
+                break
+
+            djR -= 1
+
+        return moves
+
+
